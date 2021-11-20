d419f88a93fd22e3a5f1d0200d3661e0d8981373
devhaute <devhaute@gmail.com>
2021-11-20 21:42:06 +0900

    초기 환경 세팅

---
+++ SwiftUILab3/SwiftUILab3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILab3App.swift
+//  SwiftUILab3
+//
+//  Created by devhaute on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
